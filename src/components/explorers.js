f14c0c7acea9c2f35ca10fb1cce37ed274e239a2
Aaron Smith <smitham50@gmail.com>
2019-10-08 12:44:23 -0400

    added card explorer and traderow components

---
+++ src/components/explorers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './card'
+
+function Explorers (props) {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Explorers
---
+++ src/components/traderow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './card'
+
+class TradeRow extends React.Component {
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default TradeRow
