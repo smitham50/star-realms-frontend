75ec5360749af52bcaee491ff2a4d5c187c3612f
Aaron Smith <smitham50@gmail.com>
2019-12-05 14:13:05 -0500

    added msp and mdp to card component

--- src/components/card.js
+++ src/components/card.js
@@ -3,9 +3,33 @@ import React from 'react'
 class Card extends React.Component {
   render () {
     return (
-      <div className='card'></div>
+      <div className='card'>
+
+      </div>
     )
   }
 }
 
-export default Card
+function msp(state) {
+
+  const {
+    
+  } = state.supply
+  
+
+  return {
+
+    
+
+  }
+
+}
+
+function mdp(dispatch) {
+  return {
+   
+
+  }
+}
+
+export default connect(msp, mdp)(Card)
